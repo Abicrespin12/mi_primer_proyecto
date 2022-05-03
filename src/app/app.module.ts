f2cd1ba230085b3eb6f651ec74e4ba3769555811
Abicrespin12 <crespinabii@gmail.com>
2022-05-03 11:42:16 -0300

    cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TrabajosComponent } from './pages/trabajos/trabajos.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { LogrosComponent } from './pages/logros/logros.component';
+import { FamiliaComponent } from './pages/familia/familia.component';
+import { AbigailComponent } from './pages/abigail/abigail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import {HeaderComponent} from './shared/header/header.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    TrabajosComponent,
+    MenuComponent,
+    LogrosComponent,
+    FamiliaComponent,
+    AbigailComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
